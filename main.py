81ca6b9680763302f8f3185bd2421669f4c98279
pilotleo2015 <pilotleo2015@yandex.ru>
2022-09-16 19:24:29 +0300

    добавить в первый пункт время выхода

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 from datetime import date
 import calendar
-from typing import Dict
+
+
+def main():
+#    print(schedule_for_an_even_week)
+    weekday()
+    print_menu_options()
+
+# def option2():
+#    print('Handle option \'Option 2\'')
+
+
+
+# def option3():
+#    print('Handle option \'Option 3\'')
+
+    option = int(input('Enter your choice: '))
+# Check what choice was entered and act accordingly
+    if option == 1:
+       university()
+#    elif option == 2:
+#        option2()
+#    elif option == 3:
+#        option3()
+    elif option == 4:
+        print('See you soon')
+        exit()
+#    else:
+#        print('Invalid option. Please enter a number between 1 and 4.')
 
 menu_options = {
     1: 'University',
@@ -17,36 +44,45 @@ menu_options_university = {
     5: 'Назад',
 }
 
-schedule_for_an_even_week = {  # Четная неделя сегодня
-    "Monday": "Архитектура вычислительных систем Х2(Лекция)",
-    "Tuesday": "Деловые коммуникации(1 лекция, 1 практика)",
-    "Wednesday": "Интелектуальные системы и технологии Х2(Лекция)",
-    "Thursday": "Логика и методология науки Х2(Практика)",
+schedule_for_an_even_week1 = {  # Четная неделя сегодня
+    "Monday": "Архитектура вычислительных систем Х2(Лекция)(c-20) - Нужно выйти в 16:40",
+    "Tuesday": "Деловые коммуникации(1 лекция, 1 практика)(c-20) - Нужно выйти в 16:40",
+    "Wednesday": "Интелектуальные системы и технологии Х2(Лекция)(с-20) - Нужно выйти в 16:40",
+    "Thursday": "Логика и методология науки Х2(Практика)(в-86) - Нужно выйти в ",
     "Friday": "WOW, u can chill!",
-    "Saturday": "Программная инженерия Х2(Лекция+практика)",
+    "Saturday": "Программная инженерия Х2(Лекция+практика)(В-78) - нужно выйти в 9:00",
     "Sunday": "WOW, u can chill!"
 }
 
-#schedule_for_an_even_week = {  # Четная неделя завтра
-#    "Monday": "англ",
-#    "Tuesday": "Испанский",
-#    "Wednesday": "французкий",
-#    "Thursday": "американский",
-#    "Friday": "красный",
-#    "Saturday": "зеленый",
-#    "Sunday": "черный"
-#}
-
-odd_week_schedule = {  # Нечетная неделя
-    "Monday": "",
-    "Tuesday": "gesdgg",
-    "Wednesday": "",
-    "Thursday": "",
-    "Friday": "",
-    "Saturday": "",
-    "Sunday": ""
+schedule_for_an_even_week2 = {  # Четная неделя завтра
+    "Monday": "Деловые коммуникации(1 лекция, 1 практика)(c-20) - Нужно выйти в 16:40",
+    "Tuesday": "Интелектуальные системы и технологии Х2(Лекция)(с-20) - Нужно выйти в 16:40",
+    "Wednesday": "Логика и методология науки Х2(Практика)(в-86) - Нужно выйти в",
+    "Thursday": "WOW, u can chill!",
+    "Friday": "Программная инженерия Х2(Лекция+практика)(В-78) - нужно выйти в 9:00",
+    "Saturday": "WOW, u can chill!",
+    "Sunday": "Архитектура вычислительных систем Х2(Лекция)(c-20) - Нужно выйти в 16:40"
 }
 
+odd_week_schedule = {  # Нечетная неделя сегодня
+    "Monday": "Социология и педагогика высшей школы, логика и метадология науки(лекции)(МП-1)",
+    "Tuesday": "Модели информационных процессов и систем X2(практика)(с-20)",
+    "Wednesday": "Интелектуальные системы и технологии Х2(практика)(с-20)",
+    "Thursday": "Модели информационных процессов и систем(лекция)(с-20)",
+    "Friday": "WOW, u can chill!",
+    "Saturday": "Информационная безопасность интелектуальных систем(1 практика + 2 лекции)(В-78) ",
+    "Sunday": "WOW, u can chill!"
+}
+
+odd_week_schedule = {  # Нечетная неделя завтра
+    "Monday": "Модели информационных процессов и систем X2(практика)(с-20)",
+    "Tuesday": "Интелектуальные системы и технологии Х2(практика)(с-20)",
+    "Wednesday": "Модели информационных процессов и систем(лекция)(с-20)",
+    "Thursday": "WOW, u can chill!",
+    "Friday": "Информационная безопасность интелектуальных систем(1 практика + 2 лекции)(В-78)",
+    "Saturday": "WOW, u can chill!",
+    "Sunday": "Социология и педагогика высшей школы, логика и метадология науки(лекции)(МП-1)"
+}
 
 ##############################################################################################
 
@@ -70,28 +106,29 @@ def print_menu_options_university():
     for key in menu_options_university.keys():
         print(key, '--', menu_options_university[key])
 
+
 ####################################################################
 def option_university_1():  # Четная неделя сегодня
-    print(schedule_for_an_even_week)
+#    print(schedule_for_an_even_week)
 #   print(y)
-    x = schedule_for_an_even_week.get(y)
+    x = schedule_for_an_even_week1.get(y)
     print(x)
 
 
 def option_university_2():  # Четная неделя завтра
-    print(schedule_for_an_even_week)
-    print(y)
-    x = schedule_for_an_even_week.get(y)
+#    print(schedule_for_an_even_week)
+#    print(y)
+    x = schedule_for_an_even_week2.get(y)
     print(x)
 
 def option_university_3():  # Нечетная неделя сегодня
-    print(odd_week_schedule)
+#    print(odd_week_schedule)
 #   print(y)
     x = odd_week_schedule.get(y)
     print(x)
 
 def option_university_4():  # Нечетная неделя завтра
-    print(schedule_for_an_even_week)
+#    print(schedule_for_an_even_week)
 #   print(y)
     x = odd_week_schedule.get(y)
     print(x)
@@ -104,49 +141,32 @@ def university():
     option1 = int(input('Enter your choice: '))
     if option1 == 1:
         option_university_1()
+        main()
     elif option1 == 2:
         option_university_2()
+        main()
     elif option1 == 3:
         option_university_3()
+        main()
     elif option1 == 4:
-        print('Thanks message before exiting')
-        exit()
+        option_university_4()
+        main()
+    elif option1 == 5:
+        main()
     else:
         print('Invalid option. Please enter a number between 1 and 4.')
+        university()
+#    while True:
+#        flag = input('Ещё раз? [да/нет]: ')
+#
+#        if flag == 'да':
+#            university()
+#        else:
+#            break
 
 
 
-#        print(schedule_for_an_even_week)
-#    print(y)
-#    x = schedule_for_an_even_week.get(y)
-#    print(x)
 
-def main():
-#    print(schedule_for_an_even_week)
-    weekday()
-    print_menu_options()
-
-# def option2():
-#    print('Handle option \'Option 2\'')
-
-
-
-# def option3():
-#    print('Handle option \'Option 3\'')
-
-    option = int(input('Enter your choice: '))
-# Check what choice was entered and act accordingly
-    if option == 1:
-       university()
-#    elif option == 2:
-#        option2()
-#    elif option == 3:
-#        option3()
-#    elif option == 4:
-#        print('Thanks message before exiting')
-#        exit()
-#    else:
-#        print('Invalid option. Please enter a number between 1 and 4.')
 
 main()
 
--- timemanedgment.py
+++ timemanedgment.py
@@ -1,6 +1,36 @@
 from datetime import date
 import calendar
-from typing import Dict
+
+
+
+def main():
+    print(schedule_for_an_even_week1)
+#    weekday()
+#    print_menu_options()
+
+# def option2():
+#    print('Handle option \'Option 2\'')
+
+
+
+# def option3():
+#    print('Handle option \'Option 3\'')
+
+#    option = int(input('Enter your choice: '))
+# Check what choice was entered and act accordingly
+#    if option == 1:
+#       university()
+#    elif option == 2:
+#        option2()
+#    elif option == 3:
+#        option3()
+#    elif option == 4:
+#        print('Thanks message before exiting')
+#        exit()
+#    else:
+#        print('Invalid option. Please enter a number between 1 and 4.')
+
+###################################################################################
 
 menu_options = {
     1: 'University',
@@ -17,7 +47,7 @@ menu_options_university = {
     5: 'Назад',
 }
 
-schedule_for_an_even_week = {  # Четная неделя сегодня
+schedule_for_an_even_week1 = {  # Четная неделя сегодня
     "Monday": "Архитектура вычислительных систем Х2(Лекция)",
     "Tuesday": "Деловые коммуникации(1 лекция, 1 практика)",
     "Wednesday": "Интелектуальные системы и технологии Х2(Лекция)",
@@ -27,17 +57,17 @@ schedule_for_an_even_week = {  # Четная неделя сегодня
     "Sunday": "WOW, u can chill!"
 }
 
-#schedule_for_an_even_week = {  # Четная неделя завтра
-#    "Monday": "англ",
-#    "Tuesday": "Испанский",
-#    "Wednesday": "французкий",
-#    "Thursday": "американский",
-#    "Friday": "красный",
-#    "Saturday": "зеленый",
-#    "Sunday": "черный"
-#}
+schedule_for_an_even_week2 = {  # Четная неделя завтра
+    "Monday": "Деловые коммуникации(1 лекция, 1 практика)",
+    "Tuesday": "Интелектуальные системы и технологии Х2(Лекция)",
+    "Wednesday": "Логика и методология науки Х2(Практика)",
+    "Thursday": "WOW, u can chill!",
+    "Friday": "Программная инженерия Х2(Лекция+практика)",
+    "Saturday": "WOW, u can chill!",
+    "Sunday": "Архитектура вычислительных систем Х2(Лекция)"
+}
 
-odd_week_schedule = {  # Нечетная неделя
+odd_week_schedule = {  # Нечетная неделя сегодня
     "Monday": "",
     "Tuesday": "gesdgg",
     "Wednesday": "",
@@ -47,6 +77,15 @@ odd_week_schedule = {  # Нечетная неделя
     "Sunday": ""
 }
 
+odd_week_schedule = {  # Нечетная неделя завтра
+    "Monday": "",
+    "Tuesday": "gesdgg",
+    "Wednesday": "",
+    "Thursday": "",
+    "Friday": "",
+    "Saturday": "",
+    "Sunday": ""
+}
 
 ##############################################################################################
 
@@ -72,26 +111,26 @@ def print_menu_options_university():
 
 ####################################################################
 def option_university_1():  # Четная неделя сегодня
-    print(schedule_for_an_even_week)
+#    print(schedule_for_an_even_week)
 #   print(y)
-    x = schedule_for_an_even_week.get(y)
+    x = schedule_for_an_even_week1.get(y)
     print(x)
 
 
 def option_university_2():  # Четная неделя завтра
-    print(schedule_for_an_even_week)
-    print(y)
-    x = schedule_for_an_even_week.get(y)
+#    print(schedule_for_an_even_week)
+#    print(y)
+    x = schedule_for_an_even_week2.get(y)
     print(x)
 
 def option_university_3():  # Нечетная неделя сегодня
-    print(odd_week_schedule)
+#    print(odd_week_schedule)
 #   print(y)
     x = odd_week_schedule.get(y)
     print(x)
 
 def option_university_4():  # Нечетная неделя завтра
-    print(schedule_for_an_even_week)
+#    print(schedule_for_an_even_week)
 #   print(y)
     x = odd_week_schedule.get(y)
     print(x)
@@ -104,49 +143,23 @@ def university():
     option1 = int(input('Enter your choice: '))
     if option1 == 1:
         option_university_1()
+        main()
     elif option1 == 2:
         option_university_2()
+        main()
     elif option1 == 3:
         option_university_3()
+        main()
     elif option1 == 4:
-        print('Thanks message before exiting')
-        exit()
+        option_university_4()
+        main()
     else:
         print('Invalid option. Please enter a number between 1 and 4.')
+        main()
 
 
 
-#        print(schedule_for_an_even_week)
-#    print(y)
-#    x = schedule_for_an_even_week.get(y)
-#    print(x)
 
-def main():
-    print(schedule_for_an_even_week)
-#    weekday()
-#    print_menu_options()
-
-# def option2():
-#    print('Handle option \'Option 2\'')
-
-
-
-# def option3():
-#    print('Handle option \'Option 3\'')
-
-#    option = int(input('Enter your choice: '))
-# Check what choice was entered and act accordingly
-#    if option == 1:
-#       university()
-#    elif option == 2:
-#        option2()
-#    elif option == 3:
-#        option3()
-#    elif option == 4:
-#        print('Thanks message before exiting')
-#        exit()
-#    else:
-#        print('Invalid option. Please enter a number between 1 and 4.')
 
 main()
 
